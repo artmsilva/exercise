e21a707396d23d9e9424d4019a79d85ba731a0fe
Julie <julieebacon@gmail.com>
2020-10-13 13:19:24 -0400

    few more content els

--- api/getArticle.js
+++ api/getArticle.js
@@ -1,113 +1,60 @@
 export default function getArticle() {
   return Promise.resolve([
     {
+      _id: "O23JTX787658I7CD6U3235ENM",
+      content:
+        "Space suddenly seems a little more reachable — at least, for those who have cash to burn.",
+      type: "header",
+      level: 1,
+      originalIdx: 1
+    },
+    {
+      _id: "O23JTX787658I7CD6U32YUTEXS",
+      content: "At least, for those who have cash to burn.",
+      type: "header",
+      level: 2,
+      originalIdx: 2
+    },
+    {
       _id: "O23JTQDNJZBLBI7CD6U3235ENM",
       content:
         "Space suddenly seems a little more reachable — at least, for those who have cash to burn.",
       type: "text",
-      originalIdx: 1
+      originalIdx: 3
     },
     {
       _id: "AF73UZELANBEHOM3YZA6Y6HSJM",
       content:
         "Virgin Galactic’s announcement Tuesday that it is going public through a merger with an investment firm came with an update that the company is preparing to send its first customers into space within a year, CNBC reported. More than 600 people have placed deposits topping $80 million in total, chairman Chamath Palihapitiya told the network, and another 2,500 want to get in line.",
       type: "text",
-      originalIdx: 2
+      originalIdx: 4
     },
     {
       _id: "67VJ3WYZWJHWHKG4SQM5GUDMGM",
       content:
         "Virgin isn’t alone in the space race: Blue Origin, Jeff Bezos’s space exploration company, is promoting “the largest windows in space” on its New Shepard capsule, although test flights with humans onboard have not yet taken place. (Bezos owns The Washington Post.) Elon Musk announced last year that his company, SpaceX, has a customer lined up who will pay to fly around the moon. Last month, NASA made a change in policy and said it would allow space tourists to visit the International Space Station as soon as next year. The agency said logistics would have to be arranged by SpaceX and Boeing, which NASA has tapped to get crews to the space station.",
       type: "text",
-      originalIdx: 3
+      originalIdx: 5
     },
     {
       _id: "PSZ77YVHDFAZXFVQ7F2YNOMI4E",
       content:
         "So will we all be jetting around space with our cameras, orbital passports and zero-gravity fanny packs in a decade? Not so fast. Here’s what potential space explorers need to know.",
       type: "text",
-      originalIdx: 4
+      originalIdx: 6
     },
     {
       _id: "X4O3EFY7FBC5BJOX23FPLGVOHQ",
       content: "What does space tourism involve?",
       type: "text",
-      originalIdx: 5
+      originalIdx: 7
     },
     {
       _id: "WGQAN7EXLZDUVO3M73TWA7AJZQ",
       content:
         "The most widely touted versions involve rocketing passengers more than 50 miles into the atmosphere and achieving minutes of weightlessness and witnessing Earth views before returning to land. Virgin Galactic and Blue Origin differ in the details of how they will get to space and the altitude they’ll reach, but they are promoting relatively similar experiences and plan to carry six passengers at once.",
       type: "text",
-      originalIdx: 6
-    },
-    {
-      _id: "4ZR5BJZ66RGDHM43CINSJUG6SU",
-      content:
-        "There are even more ambitious offerings: Space Adventures, which has sent seven people to space as tourists, offers multiday experiences including a “circumlunar” mission, a trip to the International Space Station and a spacewalk add-on; the company has contracted with Boeing to help sell seats aboard its spacecraft. Bigelow Space Operations, a branch of space-technology company Bigelow Aerospace, said last month it had “paid substantial sums as deposits and reservation fees to secure up to four SpaceX launches to the International Space Station.”",
-      type: "text",
       originalIdx: 8
-    },
-    {
-      _id: "55UDLLH6WNA3RACVNU7ZOUZTVQ",
-      content:
-        "For those craving weightlessness without the actual space travel, Zero Gravity Corporation gets you there 15 times, for 20 to 30 seconds each, in a trip, through aerobatic maneuvers.",
-      type: "text",
-      originalIdx: 9
-    },
-    {
-      _id: "66ZLKQSZTNG5ZLVOGAW5UVLATU",
-      content: "How much does it cost?",
-      type: "text",
-      originalIdx: 10
-    },
-    {
-      _id: "GOQO3SG4AJCWZD7BQESYQULAEY",
-      content:
-        "Virgin Galactic is reportedly charging up to $250,000 for its trips. Reuters reported that Blue Origin will charge between $200,000 and $300,000.",
-      type: "text",
-      originalIdx: 11
-    },
-    {
-      _id: "OOPXGL6GWJEAPPJPSS3IBMUTUI",
-      content:
-        "For the biggest spenders, Bigelow Space Operations has set the price of a space station trip at $52 million; most of that cost is to get there. NASA estimated that staying at the station would set travelers back about $35,000 a night.",
-      type: "text",
-      originalIdx: 12
-    },
-    {
-      _id: "CUUOZU3VSNFBHPBSIIU4FFWGI4",
-      content:
-        "Space Adventures does not list prices on its website. (If you have to ask, you probably can’t afford it.) The last tourist in space, Cirque du Soleil founder Guy Laliberté, reportedly paid $35 million for his 2009 trip to the space station arranged by the company in partnership with Russia’s space agency.",
-      type: "text",
-      originalIdx: 13
-    },
-    {
-      _id: "C4ZXLEXDGZC4PJQ4AQNFCCFMDU",
-      content:
-        "No president before had arrived with his breadth of experience: decorated Navy pilot, successful oil executive, congressman, United Nations delegate, Republican Party chairman, envoy to Beijing, director of central intelligence.",
-      type: "text",
-      originalIdx: 15
-    },
-    {
-      _id: "R5K6YF4NZZGOFK4HOGNOCNIPJU",
-      content:
-        "The option of least resistance is Zero Gravity Corporation, which sells a single seat on its flights (which, again, don’t actually go to space) for $5,400 plus tax.",
-      type: "text",
-      originalIdx: 16
-    },
-    {
-      _id: "ZZHBWKIBORAM3FNXEBJ46TDAPY",
-      content: "Who can go?",
-      type: "text",
-      originalIdx: 17
-    },
-    {
-      _id: "TOC3WPWFDFGOZMOTTDCOWZUDTA",
-      content:
-        "Other than prohibitions associated with cost, no companies have announced any limitations on who can travel. On its website, Virgin Galactic says its plan is to “open space to everybody,” from ages “spanning the teens to the 90s.”",
-      type: "text",
-      originalIdx: 18
     }
   ]);
 }
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,10 @@ Instructions:
 
 "getArticle" is a function that returns a promise that resolves to an array containing content for an article.
 
-Each of these content elements are of type "text".  Build a simple view that shows these content elements
+Each of these content elements are of type "header" or "text".  Build a simple view that shows these content elements
 using markup that is as semantic as possible.
+
+Try to break components up into small, atomic pieces.
 
 Start simple. Just render the elements with little or no style and iterate!
 */
