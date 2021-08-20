2b79f30d7fc7692dc9f6a31dece78989c7aad994
artmsilva <artmsilva@gmail.com>
2021-08-20 11:07:26 -0400

    update project to latest next react react-dom and add a readme doc

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,11 @@
-import React from "react";
-import getArticle from "../api/getArticle";
+import { getArticleByUrl } from "../api/article";
 
-/*
-Instructions: 
-
-"getArticle" is a function that returns a promise that resolves to an array containing content for an article.
-
-Each of these content elements are of type "header" or "text".  Build a simple view that shows these content elements
-using markup that is as semantic as possible.
-
-Try to break components up into small, atomic pieces.
-
-Start simple. Just render the elements with little or no style and iterate!
-*/
-
-class Article extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articleContent: []
-    };
-  }
-  componentDidMount() {
-    // HINT: something needs to go here!
-  }
-  render() {
-    return (
-      <React.Fragment>
-        {/* HINT: something needs to go here!! */}
-      </React.Fragment>
-    );
-  }
+/**
+ * Index Page
+ * @returns
+ */
+function IndexPage() {
+  return <h1>Article Goes Here.</h1>;
 }
 
-export default Article;
+export default IndexPage;
